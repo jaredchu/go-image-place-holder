2c13c360b00eba3b1914823172bf095ba4cb463c
Jared Chu <me@jaredchu.com>
2018-04-11 16:13:51 +0700

    init

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"net/http"
+	"image"
+	"image/color"
+	"image/jpeg"
+	"strings"
+	"strconv"
+)
+
+func init() {
+	http.HandleFunc("/", home)
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	width, height := getDimension(r, 100, 100)
+	img := image.NewRGBA(image.Rect(0, 0, width, height))
+	for x := 0; x < width; x++ {
+		for y := 0; y < height; y++ {
+			img.Set(x, y, color.RGBA{204, 204, 204, 1})
+		}
+	}
+
+	w.Header().Set("Content-Type", "image/jpg")
+	jpeg.Encode(w, img, nil)
+}
+
+func getDimension(r *http.Request, defaultWidth int, defaultHeight int) (int, int) {
+	path := strings.Trim(r.URL.Path,"/");
+	dArray := strings.Split(path, "x")
+	if (len(dArray) == 2) {
+		width, err := strconv.Atoi(dArray[0])
+		if (err != nil) {
+			return defaultWidth, defaultHeight
+		}
+
+		height, err := strconv.Atoi(dArray[1])
+		if (err != nil) {
+			return defaultWidth, defaultHeight
+		}
+
+		return width, height
+	} else {
+		width, err := strconv.Atoi(dArray[0])
+		if (err != nil) {
+			return defaultWidth, defaultHeight
+		}
+
+		return width, width
+	}
+}
