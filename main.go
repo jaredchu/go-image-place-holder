9af879dba129da6d04243a565941c86aad12b978
Jared Chu <me@jaredchu.com>
2018-04-11 17:33:24 +0700

    enable memcache

--- main.go
+++ main.go
@@ -8,6 +8,9 @@ import (
 	"strings"
 	"strconv"
 	"fmt"
+	"google.golang.org/appengine/memcache"
+	"bytes"
+	"google.golang.org/appengine"
 )
 
 func init() {
@@ -16,24 +19,47 @@ func init() {
 
 func home(w http.ResponseWriter, r *http.Request) {
 	width, height := getDimension(r, 100, 100)
-	if (width > 4000 || height > 4000){
-		fmt.Fprint(w,"Dismension is too big")
+	if (width > 4000 || height > 4000) {
+		fmt.Fprint(w, "Image size is too big")
 		return
 	}
 
-	img := image.NewRGBA(image.Rect(0, 0, width, height))
-	for x := 0; x < width; x++ {
-		for y := 0; y < height; y++ {
-			img.Set(x, y, color.RGBA{204, 204, 204, 1})
+	key := strconv.Itoa(width + height)
+	context := appengine.NewContext(r)
+	if item, err := memcache.Get(context, key); err == memcache.ErrCacheMiss {
+		// Not cached yet
+		img := image.NewRGBA(image.Rect(0, 0, width, height))
+		for x := 0; x < width; x++ {
+			for y := 0; y < height; y++ {
+				img.Set(x, y, color.RGBA{204, 204, 204, 1})
+			}
 		}
-	}
 
-	w.Header().Set("Content-Type", "image/jpg")
-	jpeg.Encode(w, img, nil)
+		// cache the image
+		item := &memcache.Item{
+			Key:   key,
+			Value: imgToByte(img),
+		}
+		if err := memcache.Set(context, item); err != nil {
+			fmt.Fprint(w, err)
+			return
+		}
+
+		w.Header().Set("Content-Type", "image/jpg")
+		jpeg.Encode(w, img, nil)
+
+	} else if err != nil {
+		fmt.Fprint(w, err)
+		return
+	} else {
+		// got item from memcache
+		w.Header().Set("Content-Type", "image/jpg")
+		w.Write(item.Value);
+	}
 }
 
 func getDimension(r *http.Request, defaultWidth int, defaultHeight int) (int, int) {
-	path := strings.Trim(r.URL.Path,"/");
+	path := strings.Trim(r.URL.Path, "/");
 	dArray := strings.Split(path, "x")
 	if (len(dArray) == 2) {
 		width, err := strconv.Atoi(dArray[0])
@@ -55,4 +81,13 @@ func getDimension(r *http.Request, defaultWidth int, defaultHeight int) (int, in
 
 		return width, width
 	}
+}
+
+func imgToByte(img image.Image) []byte{
+	buf := new(bytes.Buffer)
+	err := jpeg.Encode(buf, img, nil)
+	if err != nil{
+		return []byte("")
+	}
+	return buf.Bytes()
 }
